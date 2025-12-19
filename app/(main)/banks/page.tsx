546c65337571db0004f1ac7bb61bec6f0675c3e7
Coding-faizan <faizan.ahmad@devntech.com>
2025-12-19 18:14:35 +0500

    feat: my banks

--- app/(main)/banks/page.tsx
+++ app/(main)/banks/page.tsx
@@ -1,8 +1,26 @@
+import { BankCard } from '@/app/components/BankCard';
+import { ProgressBar } from '@/app/components/ProgressBar';
+import { bankCards } from '@/mocks/banks.mocks';
+
 export default function BanksPage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">My Banks</h1>
-      <p>Manage your bank accounts linked to Horizon.</p>
+    <div className="py-12 px-6 flex flex-col gap-8">
+      <div className="pl-2 space-y-4">
+        <h1 className="text-3xl font-bold space-y-5">My Banks Accounts</h1>
+        <p className="text-gray-600 text-base">
+          Effortlessly Manage Your Banking Activities.
+        </p>
+      </div>
+
+      <div className="space-y-5">
+        <div className="flex flex-wrap gap-10">
+          {bankCards.map((card, index) => (
+            <div key={index}>
+              <BankCard {...card} isCopyable={true} />
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- app/components/BankCard.tsx
+++ app/components/BankCard.tsx
@@ -1,14 +1,19 @@
-import { Nfc } from 'lucide-react';
+import { Copy, Nfc } from 'lucide-react';
 import Image from 'next/image';
 import { BankCardLines } from './BankCardLines';
+import { div } from 'framer-motion/client';
+import { ProgressBar } from './ProgressBar';
 
-interface BankCardProps {
+export interface BankCardProps {
   bankName: string;
   cardNumber: string;
   cardHolder: string;
   expiryDate: string;
   cardType: 'visa' | 'mastercard';
   variant: 'blue' | 'purple' | 'gray';
+  isCopyable?: boolean;
+  spendingLimit?: number;
+  spendingUsed?: number;
 }
 
 export const BankCard = ({
@@ -18,33 +23,74 @@ export const BankCard = ({
   expiryDate,
   cardType,
   variant,
+  isCopyable = false,
+  spendingLimit,
+  spendingUsed,
 }: BankCardProps) => {
+  const variantClasses = {
+    blue: 'bg-gradient-to-r from-blue-600 to-blue-400',
+    purple: 'bg-gradient-to-r from-purple-600 to-purple-600',
+    gray: 'bg-gradient-to-r from-gray-600 to-gray-400',
+  };
+
+  const progressStyles = {
+    blue: { progressFillColor: 'bg-primary' },
+    purple: { progressFillColor: 'bg-purple-600' },
+    gray: { progressFillColor: 'bg-gray-700' },
+  };
+
   return (
-    <div className="relative w-80 h-48 p-5 rounded-3xl text-white bg-primary">
-      <BankCardLines />
-      <p className="absolute top-5 left-5 text-base font-medium">{bankName}</p>
+    <div className="space-y-4">
+      <div
+        className={`relative w-80 h-48 p-5 rounded-3xl text-white ${variantClasses[variant]}`}
+      >
+        <BankCardLines />
+        <p className="absolute top-5 left-5 text-base font-medium">
+          {bankName}
+        </p>
 
-      <p className="absolute top-32 left-4 text-xs font-semibold font-manrope">
-        {cardHolder.toUpperCase()}
-      </p>
-      <p className="absolute top-32 left-42 text-xs font-semibold font-manrope">
-        {expiryDate}
-      </p>
+        <p className="absolute top-32 left-4 text-xs font-semibold font-manrope">
+          {cardHolder.toUpperCase()}
+        </p>
+        <p className="absolute top-32 left-42 text-xs font-semibold font-manrope">
+          {expiryDate}
+        </p>
 
-      <p className="absolute top-38 left-4 text-base font-semibold font-manrope">
-        {cardNumber}
-      </p>
+        <p className="absolute top-38 left-4 text-base font-semibold font-manrope">
+          {cardNumber}
+        </p>
 
-      <Nfc className="absolute top-5 right-5 size-6" />
+        {isCopyable ? (
+          <div className="absolute top-5 right-5 bg-white/30 flex justify-center items-center p-1.5 rounded-sm cursor-pointer">
+            <Copy className="size-3" />
+          </div>
+        ) : (
+          <Nfc className="absolute top-5 right-5 size-6" />
+        )}
 
-      <div className="absolute top-36 right-5 flex justify-center items-center bg-white/10 p-2 rounded-sm">
-        <Image
-          src={`/images/${cardType}.png`}
-          alt="Card Chip"
-          width={cardType === 'visa' ? 32 : 28}
-          height={cardType === 'visa' ? 12 : 18}
-        />
+        <div className="absolute top-36 right-5 flex justify-center items-center bg-white/10 p-2 rounded-sm">
+          <Image
+            src={`/images/${cardType}.png`}
+            alt="Card Chip"
+            width={cardType === 'visa' ? 32 : 28}
+            height={cardType === 'visa' ? 12 : 18}
+          />
+        </div>
       </div>
+
+      {spendingLimit && spendingUsed !== undefined && (
+        <div className="space-y-2 px-1">
+          <div className="flex justify-between">
+            <p className="text-gray-700 font-medium">Spending this month</p>
+            <p className="text-gray-600 text-sm">${spendingUsed}</p>
+          </div>
+          <ProgressBar
+            progress={(spendingUsed / spendingLimit) * 100}
+            progressBgColor="bg-gray-200"
+            progressFillColor={progressStyles[variant].progressFillColor}
+          />
+        </div>
+      )}
     </div>
   );
 };
